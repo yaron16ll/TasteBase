096c088c83ee73fe37aac8973eae8cc48d84062f
yaron16ll <60570643+yaron16ll@users.noreply.github.com>
2024-08-01 00:45:01 +0300

    finished

---
+++ app/src/main/java/com/example/tastebase/views/activities/Dish.java
@@ -0,0 +1,17 @@
+package com.example.tastebase.views.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tastebase.R;
+
+public class Dish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.dish_activity);
+
+    }
+}
